260fb052ddb112a3fe7b1c6034c38f0a00abaa36
subroto18 <subroto@revivotech.in>
2024-12-29 16:01:34 +0530

    fixed login bug

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("auth");
+  const loggedInUserEmail = localStorage.getItem("loggedInUserEmail");
+  const loggedInUserName = localStorage.getItem("loggedInUserName");
+  const auth = JSON.parse(localStorage.getItem("auth"));
+
+  let token = false;
+
+  if (loggedInUserEmail) {
+    if (auth) {
+      const LoggedInUser = auth?.filter(
+        (item) =>
+          item.email === loggedInUserEmail && item.name === loggedInUserName
+      );
+      if (LoggedInUser?.length > 0) {
+        token = true;
+      }
+    }
+  }
+
   return token ? children : <Navigate to="/auth" />;
 };
 
